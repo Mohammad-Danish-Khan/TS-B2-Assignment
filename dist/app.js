522135b9e19621ada8ba75f50328fa40f8934ab8
Mohammad-Danish-Khan <mohammad.danish3090@gmail.com>
2023-04-02 21:03:03 +0500

    assignment 2

--- dist/app.js
+++ dist/app.js
@@ -1,24 +1,88 @@
 "use strict";
-const xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos');
-xhr.send();
-xhr.onload = () => {
-    if (xhr.status === 200) {
-        const data = JSON.parse(xhr.responseText);
-        console.log(data);
-        const table = document.createElement("table");
-        data.forEach((item) => {
-            const row = table.insertRow();
-            const cell1 = row.insertCell();
-            const cell2 = row.insertCell();
-            const cell3 = row.insertCell();
-            cell1.innerHTML = item.id;
-            cell2.innerHTML = item.title;
-            cell3.innerHTML = item.completed;
-        });
-        document.body.appendChild(table);
+class Vehicle {
+    constructor(make, model, year, rented) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.rented = rented;
     }
-    else {
-        console.error('Error fetching data');
+    getMake() {
+        return this.make;
     }
-};
+    getModel() {
+        return this.model;
+    }
+    getYear() {
+        return this.year;
+    }
+    isRented() {
+        return this.rented;
+    }
+    setRented(rented) {
+        this.rented = rented;
+    }
+    rent() {
+        if (this.rented) {
+            return "Sorry, this vehicle is already rented.";
+        }
+        else {
+            this.rented = true;
+            return "Vehicle rented successfully.";
+        }
+    }
+    returnVehicle() {
+        if (this.rented) {
+            this.rented = false;
+            return "Vehicle returned successfully.";
+        }
+        else {
+            return "This vehicle is not rented.";
+        }
+    }
+}
+class Car extends Vehicle {
+    constructor(make, model, year, rented, numSeats, hasAC) {
+        super(make, model, year, rented);
+        this.numSeats = numSeats;
+        this.hasAC = hasAC;
+    }
+    rentalRate() {
+        return 50;
+    }
+    hasAirConditioning() {
+        return this.hasAC;
+    }
+    getNumSeats() {
+        return this.numSeats;
+    }
+}
+class Truck extends Vehicle {
+    constructor(make, model, year, rented, maxPayload) {
+        super(make, model, year, rented);
+        this.maxPayload = maxPayload;
+    }
+    rentalRate() {
+        return 100;
+    }
+    getMaxPayload() {
+        return this.maxPayload;
+    }
+}
+class Motorcycle extends Vehicle {
+    constructor(make, model, year, rented, hasSidecar) {
+        super(make, model, year, rented);
+        this.hasSidecar = hasSidecar;
+    }
+    rentalRate() {
+        return 30;
+    }
+    hasSideCar() {
+        return this.hasSidecar;
+    }
+}
+const button = document.getElementById('vehicle');
+button === null || button === void 0 ? void 0 : button.addEventListener('click', function handleClick(event) {
+    console.log('button clicked');
+    console.log(event);
+    console.log(event.target);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,123 @@
-const xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos');
-xhr.send();
-
-xhr.onload = () => {
-    if (xhr.status === 200) {
-        const data = JSON.parse(xhr.responseText);
-        console.log(data);
-
-        const table = document.createElement("table");
-
-        data.forEach((item: any) => {
-            const row = table.insertRow();
-            const cell1 = row.insertCell();
-            const cell2 = row.insertCell();
-            const cell3 = row.insertCell();
-            cell1.innerHTML = item.id;
-            cell2.innerHTML = item.title;
-            cell3.innerHTML = item.completed;
-        });
-
-        document.body.appendChild(table);
-    } else {
-        console.error('Error fetching data');
+abstract class Vehicle {
+    make: string;
+    model: string;
+    year: number;
+    rented: boolean;
+  
+    constructor(make: string, model: string, year: number, rented: boolean) {
+      this.make = make;
+      this.model = model;
+      this.year = year;
+      this.rented = rented;
     }
-};
+  
+    getMake(): string {
+      return this.make;
+    }
+  
+    getModel(): string {
+      return this.model;
+    }
+  
+    getYear(): number {
+      return this.year;
+    }
+  
+    isRented(): boolean {
+      return this.rented;
+    }
+  
+    setRented(rented: boolean): void {
+      this.rented = rented;
+    }
+  
+    abstract rentalRate(): number;
+  
+    rent(): string {
+      if (this.rented) {
+        return "Sorry, this vehicle is already rented.";
+      } else {
+        this.rented = true;
+        return "Vehicle rented successfully.";
+      }
+    }
+  
+    returnVehicle(): string {
+      if (this.rented) {
+        this.rented = false;
+        return "Vehicle returned successfully.";
+      } else {
+        return "This vehicle is not rented.";
+      }
+    }
+  }
+  
+  class Car extends Vehicle {
+    numSeats: number;
+    hasAC: boolean;
+  
+    constructor(make: string, model: string, year: number, rented: boolean, numSeats: number, hasAC: boolean) {
+      super(make, model, year, rented);
+      this.numSeats = numSeats;
+      this.hasAC = hasAC;
+    }
+  
+    rentalRate(): number {
+      return 50;
+    }
+  
+    hasAirConditioning(): boolean {
+      return this.hasAC;
+    }
+  
+    getNumSeats(): number {
+      return this.numSeats;
+    }
+  }
+  
+  class Truck extends Vehicle {
+    maxPayload: number;
+  
+    constructor(make: string, model: string, year: number, rented: boolean, maxPayload: number) {
+      super(make, model, year, rented);
+      this.maxPayload = maxPayload;
+    }
+  
+    rentalRate(): number {
+      return 100;
+    }
+  
+    getMaxPayload(): number {
+      return this.maxPayload;
+    }
+  }
+  
+  class Motorcycle extends Vehicle {
+    hasSidecar: boolean;
+  
+    constructor(make: string, model: string, year: number, rented: boolean, hasSidecar: boolean) {
+      super(make, model, year, rented);
+      this.hasSidecar = hasSidecar;
+    }
+  
+    rentalRate(): number {
+      return 30;
+    }
+  
+    hasSideCar(): boolean {
+      return this.hasSidecar;
+    }
+  }
 
+  const button = document.getElementById('vehicle');
+
+  button?.addEventListener('click', function handleClick(event) {
+    let make = document.getElementById('make');
+    let model = document.getElementById('model');
+    let year = document.getElementById('year');
+    let numSeats = document.getElementById('seats');
+    let isRented = document.getElementById('rented');
+    let hasAC = document.getElementById('hasAc');
+    let car = new Car(make, model, year, numSeats, isRented, hasAC);
+
+  });
